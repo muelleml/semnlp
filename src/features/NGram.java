7afabc1da01587704e6606d4699e04120395d5af
muelleml <muelleml>
2012-12-08 00:48:11 +0100

    added N-Gram Feature

--- src/features/NGram.java
+++ src/features/NGram.java
@@ -13,7 +13,43 @@ import model.Word;
  * @author muelleml
  * 
  */
+/**
+ * @author muelleml
+ * 
+ */
 public class NGram implements Feature {
+
+	private int min;
+	private int max;
+	private int length;
+	private boolean direction;
+	private int rel;
+
+	/**
+	 * Extracts N-Grams from a Word. Missing Characters will be filled with
+	 * blankspaces.
+	 * 
+	 * @param min
+	 *            The minimal length of the N-Grams
+	 * @param max
+	 *            The maximal length of the N-Grams
+	 * @param length
+	 *            The length of the Substring that will be processed. Shortening
+	 *            will happen after reversal and filling.
+	 * @param direction
+	 *            The direction for the N-Gram Generation. False means from back
+	 *            to front. e.g. instead of processing "hello", the String will
+	 *            be taken as "olleH". The String will first be reversed an then
+	 *            filled
+	 */
+	public NGram(int min, int max, int length, boolean direction) {
+
+		this.min = min;
+		this.max = max;
+		this.length = length;
+		this.direction = direction;
+		this.rel = 0;
+	}
 
 	/*
 	 * (non-Javadoc)
@@ -22,9 +58,62 @@ public class NGram implements Feature {
 	 */
 	@Override
 	public List<String> extract(Word w, Sentence s) {
+
 		List<String> r = new LinkedList<String>();
-		
+		StringBuilder sb = new StringBuilder(w.word);
+
+		// reversal
+		if (!direction) {
+			sb.reverse();
+		}
+
+		// filling
+		while (sb.length() <= length) {
+			sb.append(" ");
+		}
+
+		// shortening
+		sb = new StringBuilder(sb.substring(0, length));
+
+		return gram(sb.toString(), min, max);
+	}
+
+	private List<String> gram(String l, int m, int n) {
+
+		List<String> r = new LinkedList<String>();
+
+		// check if maximum depth is reached
+		if (n - m < 1) {
+
+			int i = 0;
+
+			while (n + i <= l.length()) {
+				String t = l.substring(i, n + i);
+
+				r.add(t);
+				r.addAll(gram(t, m, n - 1));
+
+				i++;
+			}
+
+		}
+
 		return r;
+
+	}
+
+	private int getPosition(Word w, Sentence s) {
+		int i = 0;
+
+		for (Word tW : s.words) {
+
+			if (tW.tokenID == w.tokenID) {
+				i = s.words.indexOf(tW);
+			}
+
+		}
+
+		return i;
 	}
 
 }
