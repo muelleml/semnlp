baf239639ab59986df36e5edab742cf846c1d77b
Patrik <Patrik@141.58.61.98>
2013-01-16 15:19:12 +0100

    asd

--- src/features/scope/ScopeFeature.java
+++ src/features/scope/ScopeFeature.java
@@ -1,11 +1,12 @@
 package features.scope;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import model.Sentence;
 
 public interface ScopeFeature {
 
-	public ScopeFeatureValue extractTrain(Sentence s);
-	public List<List<String>> extractClassif(Sentence s);
+	public ScopeFeatureValue[] extractTrain(Sentence s);
+	public ArrayList<List<List<String>>> extractClassif(Sentence s);
 }
